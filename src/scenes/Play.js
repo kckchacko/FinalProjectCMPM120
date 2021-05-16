a17882f500ffcdeadaddfd777e44dc5c6316dddc
Kevin <kchacko@ucsc.edu>
2021-05-15 17:46:19 -0700

    updated the index

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,9 +8,12 @@ export default class Play extends Phaser.scene{
     }
     create(){
         
+        this.hero = new Hero(this, 200, 120, 'unicorn', 100).setScale(0.5)
+        this.cameras.main.startFollow(this.hero, true, 0.8, 0.8)
+        this.hero.body.setCollideWorldBounds(true)
         
     }
     update(time, delta){
-        
+
     }
 }
